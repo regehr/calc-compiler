521ce3cf95352c87d7e82905a599bf0f3aff7472
John Regehr <regehr@cs.utah.edu>
2016-10-18 19:18:54 -0600

    add driver

--- calcc.cpp
+++ calcc.cpp
@@ -1,13 +1,13 @@
 #include "llvm/ADT/APInt.h"
 #include "llvm/IR/ConstantRange.h"
 #include "llvm/IR/IRBuilder.h"
-#include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Type.h"
 #include "llvm/IR/Verifier.h"
-#include <iostream>
+#include "llvm/Support/raw_ostream.h"
 #include <cassert>
+#include <iostream>
 
 using namespace llvm;
 using namespace std;
@@ -18,11 +18,9 @@ static std::unique_ptr<Module> M = llvm::make_unique<Module>("calc", C);
 
 static int compile() {
   M->setTargetTriple(llvm::sys::getProcessTriple());
-  std::vector<Type*> SixInts(6, Type::getInt64Ty(C));
-  FunctionType *FT =
-    FunctionType::get(Type::getInt64Ty(C), SixInts, false);
-  Function *F =
-    Function::Create(FT, Function::ExternalLinkage, "f", &*M);
+  std::vector<Type *> SixInts(6, Type::getInt64Ty(C));
+  FunctionType *FT = FunctionType::get(Type::getInt64Ty(C), SixInts, false);
+  Function *F = Function::Create(FT, Function::ExternalLinkage, "f", &*M);
   BasicBlock *BB = BasicBlock::Create(C, "entry", F);
   Builder.SetInsertPoint(BB);
 
@@ -36,6 +34,4 @@ static int compile() {
   return 0;
 }
 
-int main(void) {
-  return compile();
-}
+int main(void) { return compile(); }
---
+++ driver.c
@@ -0,0 +1,23 @@
+extern long f(long, long, long, long, long, long);
+
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  assert(sizeof(long) == 8);
+  assert(argc <= 7);
+  long a[6] = {0};
+  int i;
+  for (i = 1; i < argc; i++) {
+    errno = 0;
+    a[i - 1] = strtol(argv[i], 0, 10);
+    assert(errno == 0);
+  }
+  printf("args = %ld %ld %ld %ld %ld %ld\n", a[0], a[1], a[2], a[3], a[4],
+         a[5]);
+  long res = f(a[0], a[1], a[2], a[3], a[4], a[5]);
+  printf("result = %ld\n", res);
+  return 0;
+}
